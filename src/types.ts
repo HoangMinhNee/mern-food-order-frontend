bf04a573a89b17f8de4eeedea7e1cbeeaea3adb7
Hoang Minh <hoangminhh.281102@gmail.com>
2024-10-09 15:00:13 +0700

    checkout deploy

--- src/types.ts
+++ src/types.ts
@@ -13,6 +13,13 @@ export type MenuItem = {
   price: number;
 };
 
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
 export type Restaurant = {
   _id: string;
   user: string;
@@ -25,6 +32,27 @@ export type Restaurant = {
   menuItems: MenuItem[];
   imageUrl: string;
   lastUpdated: string;
+};
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
 };
 
 export type RestaurantSearchResponse = {
